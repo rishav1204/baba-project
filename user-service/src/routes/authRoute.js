09174f60a511ec005ff40fd6505d416304612930
rishav1204 <kumar.rishav200176@gmail.com>
2025-04-24 10:55:47 +0530

    Implemented sign-up using e-mail

--- user-service/src/routes/authRoute.js
+++ user-service/src/routes/authRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { AuthController } from '../controllers/authController.js';
+import { validateSignup } from '../middlewares/validationMiddleware.js';
 
 const router = express.Router();
 
 // Auth Routes - No authentication required
-router.post('/signup/email', authController.signupWithEmail); /*-> authRoutes*/ 
-router.post('/signup/google', authController.signupWithGoogle); /*-> authRoutes*/
+router.post('/signup/email', validateSignup, AuthController.signupWithEmail); /*-> authRoutes*/ 
+router.post('/signup/google', AuthController.signupWithGoogle); /*-> authRoutes*/
 
 export default router;
