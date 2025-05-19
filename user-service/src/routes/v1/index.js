2600a07c26c5adb5468b34f202de43e78a6107a0
Saket.Suman <saket.suman@bighaat.com>
2025-05-19 18:04:54 +0530

    added login route

--- user-service/src/routes/v1/index.js
+++ user-service/src/routes/v1/index.js
@@ -1,7 +1,10 @@
 import express from "express";
-
 import { AuthController } from "../../controllers/index.js";
-import { validateSignup } from "../../middlewares/validationMiddleware.js";
+import {
+  validateSignup,
+  validateGoogleSignup,
+} from "../../middlewares/validationMiddleware.js";
+import { loginWithEmails } from "../../controllers/authController.js";
 const router = express.Router();
 
 router.post(
@@ -9,4 +12,10 @@ router.post(
   validateSignup,
   AuthController.signupWithEmail
 );
+router.post(
+  "/auth/signup/google",
+  validateGoogleSignup,
+  AuthController.signupWithGoogle
+); /*-> authRoutes*/
+router.post("/auth/login", loginWithEmails); /*-> authRoutes*/
 export default router;
